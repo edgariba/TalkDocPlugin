cfb2d6be65f5607f996518973470790156f1887f
Edgar Barrera <ebarrera@americadigital.org>
2020-03-31 13:44:05 -0600

    TalkDocPlugin

--- www/plugin.js
+++ www/plugin.js
@@ -6,7 +6,7 @@ var PLUGIN_NAME = 'TalkDocPlugin';
 var TalkDocPlugin = {
   saludo: function (name, successCallback, errorCallback){
         exec(successCallback, errorCallback, PLUGIN_NAME, "saludar", [name]);
-  }
+  },
 
   llamada: function (argumentos, successCallback, errorCallback){
           exec(successCallback, errorCallback, PLUGIN_NAME, "llamar", [argumentos]);
